6d15231fb3402611a681468c66ad72d6c644bb05
btrif <bogdan.evanzo@gmail.com>
2023-03-17 11:12:53 +0200

    JS repo 1

---
+++ scripts/toggle_button1.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+    const backgroundColor = btn.style.backgroundColor;
+
+    if (backgroundColor === 'salmon') {
+        btn.style.backgroundColor = 'green';
+
+        // 👇️ optionally change text color
+        // btn.style.color = 'white';
+    } else if (backgroundColor === "green") {
+        btn.style.backgroundColor = 'salmon';
+
+        // 👇️ optionally change text color
+        // btn.style.color = 'blue';
+    }
+
+else{
+    btn.style.backgroundColor = 'salmon';
+}
+
+});
